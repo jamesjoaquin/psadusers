180360a3a0b4027f7572d43452a8ff85a746bfee
jamesjoaquin <42815264+jamesjoaquin@users.noreply.github.com>
2018-09-19 14:01:25 -0600

    Add files via upload

---
+++ UserNames.ps1
@@ -0,0 +1,18 @@
+﻿#This prompts for an AD group, outputs a count of users in the group, and prompts to run again or quit#
+
+do {
+
+
+$group = Read-Host -Prompt group
+
+Get-ADGroupMember "$group" | Where objectclass -eq "user" | select name | export-csv -Path $env:USERPROFILE\Desktop\$group.csv -NoClobber
+
+
+
+
+$response = Read-Host "Again?"
+
+
+}
+
+while ($response -eq "y")
